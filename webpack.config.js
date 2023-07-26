9e015238e5961a1e64f88cb31f498b99c756dd1f
vsompura3 <vsompura3@gmail.com>
2023-07-26 18:11:56 +0530

    added plugins and configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: { bundle: path.resolve(__dirname, 'src', 'index.js') },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name][contenthash].js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack boilerplate',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
 }
